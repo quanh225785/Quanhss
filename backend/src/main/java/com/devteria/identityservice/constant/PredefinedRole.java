d3ddf150c26d692f243c6d2d3c74a5876acec863
Quanh <nqa12092001@gmail.com>
2025-11-19 12:40:03 +0700

    init

---
+++ backend/src/main/java/com/devteria/identityservice/constant/PredefinedRole.java
@@ -0,0 +1,8 @@
+package com.devteria.identityservice.constant;
+
+public class PredefinedRole {
+    public static final String USER_ROLE = "USER";
+    public static final String ADMIN_ROLE = "ADMIN";
+    public static final String AGENT_ROLE = "AGENT";
+    private PredefinedRole() {}
+}
